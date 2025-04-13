a08187737bda4251c70f75d232890f58e2b521a3
Milan08S <miguesanchez82003@gmail.com>
2025-04-13 01:59:58 -0400

    [NEW] Redux logic for cart added

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,38 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.forEach(element => {
+        total =+ parseFloat(element.quantity) * parseFloat(element.cost.substring(1));
+        
+    });
+    return total;
   };
+
+    const handleCheckoutShopping = (e) => {
+        alert('Functionality to be added for future reference');
+    };
 
   const handleContinueShopping = (e) => {
-    
+    onContinueShopping(e)
   };
 
-
-
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if(item.quantity-- == 0) handleRemove(item);
+    else dispatch(updateQuantity({name: item.name, quantity: item.quantity - 1}));
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return parseFloat(item.quantity) * parseFloat(item.cost.substring(1));
   };
 
   return (
